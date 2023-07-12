2b426cd6703372e698f4f122a014334178ddc0f2
Duytapcode <baoduy162cr@gmail.com>
2023-07-13 01:28:11 +0700

    Admin Dashboard Commit

--- src/main/java/com/example/projectsem2/RunApplication.java
+++ src/main/java/com/example/projectsem2/RunApplication.java
@@ -7,12 +7,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class RunApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(RunApplication.class.getResource("dashboard-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
     }
